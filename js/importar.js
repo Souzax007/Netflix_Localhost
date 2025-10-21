4b54af85e766e535b0889d58acf45aab2ccaa437
Marcos <marcos@albesbrasil.com.br>
2025-10-21 17:08:11 -0300

    aplicações necessárias

--- js/importar.js
+++ js/importar.js
@@ -1,3 +1,2 @@
 import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
-
 new DotLottie({autoplay: true,loop: true,canvas: document.getElementById("canvas"),src: "./json/logo/Movie Theatre.json", });
